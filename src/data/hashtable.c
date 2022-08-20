76e19e469a180e8b03b5b57a98ec16a77c919df1
joseph calderon <calderon.joe@gmail.com>
2022-08-20 14:53:36 -0700

    replace rb tree based hashtable with rax

--- src/data/hashtable.c
+++ src/data/hashtable.c
@@ -18,285 +18,103 @@
 
 #include "hashtable.h"
 
-static int tg_hashtable_cmp(const tg_hashtable_key *k1, const tg_hashtable_key *k2);
-
-RB_GENERATE(tg_hashtable_rbtree, tg_hashtable_key, entry, tg_hashtable_cmp);
-
 tg_hashtable *tg_hashtable_alloc(size_t buckets, void (*callback)(void *value))
 {
-	tg_hashtable *hashtable;
-	tg_hashtable_bucket *bucket;
-	size_t i, j;
+    tg_hashtable *hashtable;
 
-	assert(buckets > 0);
+    assert(buckets > 0);
 
-	hashtable = malloc(sizeof(tg_hashtable));
+    hashtable = malloc(sizeof(tg_hashtable));
 
-	assert(hashtable);
+    assert(hashtable);
 
-	hashtable->magic = TG_HASHTABLE_MAGIC;
-	hashtable->bucket_len = buckets;
-	hashtable->callback = callback;
+    hashtable->magic = TG_HASHTABLE_MAGIC;
+    hashtable->callback = callback;
+    hashtable->dict = raxNew();
 
-	hashtable->buckets = malloc(hashtable->bucket_len * sizeof(tg_hashtable_bucket));
+    assert(hashtable->dict);
 
-	assert(hashtable->buckets);
-
-	for(i = 0; i < hashtable->bucket_len; i++)
-	{
-		bucket = &(hashtable->buckets[i]);
-
-		bucket->magic = TG_HASHTABLE_BUCKET_MAGIC;
-		bucket->prealloc_len = TG_HASHTABLE_PREALLOC_LEN;
-
-		RB_INIT(&bucket->rbtree);
-
-		bucket->size = 0;
-
-		for(j = 0; j < bucket->prealloc_len; j++)
-		{
-			bucket->prealloc[j].magic = 0;
-		}
-	}
-
-	return hashtable;
+    return hashtable;
 }
 
-static tg_hashtable_bucket *tg_hashtable_hash_djb2(const tg_hashtable *hashtable, const char *str)
+void *tg_hashtable_get2(tg_hashtable *hashtable, const char *key, size_t key_len)
 {
-	unsigned long hash = 5381;
-	int c;
+    void *ret = NULL;
 
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
-	assert(hashtable->bucket_len);
-	assert(str);
+    assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
+    assert(key);
 
-	while((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+    ret = raxFind(hashtable->dict, (unsigned char *)key, key_len);
 
-	return &(hashtable->buckets[hash % hashtable->bucket_len]);
-}
-
-static int tg_hashtable_cmp(const tg_hashtable_key *k1, const tg_hashtable_key *k2)
-{
-	return strcmp(k1->key, k2->key);
-}
-
-static tg_hashtable_key *tg_hashtable_key_alloc(tg_hashtable_bucket *bucket)
-{
-	tg_hashtable_key *key;
-	size_t i;
-
-	assert(bucket && bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-	for(i = 0; i < bucket->prealloc_len; i++)
-	{
-		key = &bucket->prealloc[i];
-
-		if(!key->magic)
-		{
-			key->magic = TG_HASHTABLE_KEY_MAGIC;
-			key->malloc = 0;
-
-			return key;
-		}
-	}
-
-	key = malloc(sizeof(tg_hashtable_key));
-
-	assert(key);
-
-	key->magic = TG_HASHTABLE_KEY_MAGIC;
-	key->malloc = 1;
-
-	return key;
-}
-
-static void tg_hashtable_key_free(tg_hashtable_key *key)
-{
-	assert(key && key->magic == TG_HASHTABLE_KEY_MAGIC);
-
-	key->magic = 0;
-
-	if(key->malloc)
-	{
-		free(key);
-	}
+    return ret == raxNotFound ? NULL : ret;
 }
 
 void *tg_hashtable_get(tg_hashtable *hashtable, const char *key)
 {
-	tg_hashtable_bucket *bucket;
-	tg_hashtable_key *result, find;
-	void *ret = NULL;
-
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
-	assert(key);
-
-	find.key = key;
-
-	bucket = tg_hashtable_hash_djb2(hashtable, key);
-
-	assert(bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-	result = RB_FIND(tg_hashtable_rbtree, &bucket->rbtree, &find);
-
-	if(result)
-	{
-		assert(result->magic == TG_HASHTABLE_KEY_MAGIC);
-
-		ret = result->value;
-	}
-
-	return ret;
+    return tg_hashtable_get2(hashtable, key, strlen(key));
 }
 
 void tg_hashtable_set(tg_hashtable *hashtable, const char *key, void *value)
 {
-	tg_hashtable_bucket *bucket;
-	tg_hashtable_key *add, *ret;
+    assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
+    assert(key);
+    assert(value);
 
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
-	assert(key);
-	assert(value);
+    void *old = NULL;
+    raxInsert(hashtable->dict, (unsigned char *)key, strlen(key), value, &old);
 
-	bucket = tg_hashtable_hash_djb2(hashtable, key);
-
-	assert(bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-	add = tg_hashtable_key_alloc(bucket);
-
-	add->key = key;
-	add->value = value;
-
-	ret = RB_INSERT(tg_hashtable_rbtree, &bucket->rbtree, add);
-
-	if(ret)
-	{
-		if(hashtable->callback)
-		{
-			hashtable->callback(ret->value);
-		}
-
-		tg_hashtable_key_free(add);
-
-		assert(ret->magic == TG_HASHTABLE_KEY_MAGIC);
-
-		ret->key = key;
-		ret->value = value;
-	}
-	else
-	{
-		bucket->size++;
-	}
+    if (old) {
+        if (hashtable->callback) {
+            hashtable->callback(old);
+        }
+    }
 }
 
 int tg_hashtable_delete(tg_hashtable *hashtable, const char *key)
 {
-	tg_hashtable_bucket *bucket;
-	tg_hashtable_key *result, find;
-	int ret = 0;
+    int ret = 0;
 
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
-	assert(key);
+    assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
+    assert(key);
 
-	find.key = key;
+    void *old = NULL;
+    int removed = raxRemove(hashtable->dict, (unsigned char *)key, strlen(key), &old);
+    if (removed) {
+        if (hashtable->callback) {
+            hashtable->callback(old);
+        }
+        ret = 1;
+    }
 
-	bucket = tg_hashtable_hash_djb2(hashtable, key);
-
-	assert(bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-	result = RB_FIND(tg_hashtable_rbtree, &bucket->rbtree, &find);
-
-	if(result)
-	{
-		assert(result->magic == TG_HASHTABLE_KEY_MAGIC);
-
-		RB_REMOVE(tg_hashtable_rbtree, &bucket->rbtree, result);
-
-		if(hashtable->callback)
-		{
-			hashtable->callback(result->value);
-		}
-
-		tg_hashtable_key_free(result);
-
-		bucket->size--;
-
-		ret = 1;
-	}
-
-	return ret;
+    return ret;
 }
 
 size_t tg_hashtable_size(tg_hashtable *hashtable)
 {
-	tg_hashtable_bucket *bucket;
-	size_t i, size = 0;
+    size_t size = 0;
 
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
+    assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
 
-	for(i = 0; i < hashtable->bucket_len; i++)
-	{
-		bucket = &hashtable->buckets[i];
+    size = raxSize(hashtable->dict);
 
-		assert(bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-		size += bucket->size;
-	}
-
-	return size;
+    return size;
 }
 
 void tg_hashtable_free(tg_hashtable *hashtable)
 {
-	tg_hashtable_bucket *bucket;
-	tg_hashtable_key *key, *next;
-	size_t i;
+    if (!hashtable) {
+        return;
+    }
 
-	if(!hashtable)
-	{
-		return;
-	}
+    assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
 
-	assert(hashtable && hashtable->magic == TG_HASHTABLE_MAGIC);
+    if (hashtable->callback) {
+        raxFreeWithCallback(hashtable->dict, hashtable->callback);
+    } else {
+        raxFree(hashtable->dict);
+    }
 
-	for(i = 0; i < hashtable->bucket_len; i++)
-	{
-		bucket = &(hashtable->buckets[i]);
+    hashtable->magic = 0;
 
-		assert(bucket->magic == TG_HASHTABLE_BUCKET_MAGIC);
-
-		RB_FOREACH_SAFE(key, tg_hashtable_rbtree, &bucket->rbtree, next)
-		{
-			RB_REMOVE(tg_hashtable_rbtree, &bucket->rbtree, key);
-
-			assert(key->magic == TG_HASHTABLE_KEY_MAGIC);
-
-			if(hashtable->callback)
-			{
-				hashtable->callback(key->value);
-			}
-
-			tg_hashtable_key_free(key);
-
-			bucket->size--;
-		}
-
-		assert(!bucket->size);
-
-		RB_INIT(&bucket->rbtree);
-
-		bucket->magic = 0;
-	}
-
-	free(hashtable->buckets);
-	hashtable->bucket_len = 0;
-	hashtable->buckets = NULL;
-
-	hashtable->magic = 0;
-
-	free(hashtable);
+    free(hashtable);
 }
--- src/data/hashtable.h
+++ src/data/hashtable.h
@@ -28,61 +28,20 @@
 #define assert(e)
 #endif
 
-#include "tree.h"
-
-
-#define TG_HASHTABLE_PREALLOC_LEN	2
-
-
-typedef struct tg_hashtable_key
-{
-	unsigned int			magic;
-#define	TG_HASHTABLE_KEY_MAGIC		0x4D2FA1FF
-
-	RB_ENTRY(tg_hashtable_key)	entry;
-
-	const char			*key;
-	void				*value;
-
-	int				malloc:1;
-}
-tg_hashtable_key;
-
-
-typedef RB_HEAD(tg_hashtable_rbtree, tg_hashtable_key) tg_hashtable_rbtree;
-
-
-typedef struct
-{
-	unsigned int			magic;
-#define	TG_HASHTABLE_BUCKET_MAGIC	0xA11FB208
-
-	tg_hashtable_rbtree		rbtree;
-
-	size_t				size;
-	size_t				prealloc_len;
-
-	tg_hashtable_key		prealloc[TG_HASHTABLE_PREALLOC_LEN];
-}
-tg_hashtable_bucket;
-
+#include "rax.h"
 
 typedef struct
 {
 	unsigned int			magic;
 #define	TG_HASHTABLE_MAGIC		0x815BDDAB
-
-	tg_hashtable_bucket		*buckets;
-
-	size_t				bucket_len;
-
+	rax *dict;
 	void				(*callback)(void*);
 }
 tg_hashtable;
 
-
 tg_hashtable *tg_hashtable_alloc(size_t buckets, void (*callback)(void *value));
 void *tg_hashtable_get(tg_hashtable *hashtable, const char *key);
+void *tg_hashtable_get2(tg_hashtable *hashtable, const char *key, size_t key_len);
 void tg_hashtable_set(tg_hashtable *hashtable, const char *key, void *value);
 int tg_hashtable_delete(tg_hashtable *hashtable, const char *key);
 size_t tg_hashtable_size(tg_hashtable *hashtable);
